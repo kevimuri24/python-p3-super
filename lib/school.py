b6c9638d9b24b377f03f61752e6011812a40d3f7
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-10-09 23:43:18 -0400

    Finished Lab

--- lib/school.py
+++ lib/school.py
@@ -1,10 +1,22 @@
-class User:
+class user:
+    def __init__(self, name):
+        print("User.__init__called.")
+        self.name = name
+        
+        
     def log_in(self):
         self.logged_in = True
         
-class Student(User):
+class student(user):
+    def __init__(self, name, grade):
+        print("Student.__init__called.")
+        super().__init__(name)
+        self.grade = grade
+        
     def log_in(self):
         super().log_in()
         self.in_class = True
         
-print(issubclass(Student, User))  # Output: True
+oneil = student("O'neil", 10)
+print(oneil.name)  # Output: O'neil
+print(oneil.grade)  # Output: 10
